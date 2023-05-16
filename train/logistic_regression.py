993d0db6c731c4ebab5bd9bda5569a303a52be49
Abduragim Shtanchaev <shtanchaev1994@gmail.com>
2023-05-16 21:36:14 +0300

    fix bug

--- train/logistic_regression.py
+++ train/logistic_regression.py
@@ -11,16 +11,19 @@ def run_logistic_regression(cfg):
         data, labels = generate_syntetic_data(cfg.N_samples, cfg.N_rois,  cfg.classes)
     elif cfg.dataset == "cobre":
         logger.info("Genereting Cobre data set for Logistic regression")
-        data, labels = generate_cobra_data()
+        data, labels = generate_cobra_data(cfg)
 
     lr = LogisticRegression(penalty='l1', solver='liblinear')
 
     scores = cross_val_score(
         lr,
-        data.reshape(cfg.N_samples, -1),
+        data.reshape(data.shape[0], -1),
         labels,
         cv=5,
-        scoring='accuracy')
+        scoring='f1_weighted',
+        n_jobs=-1
+        )
 
     logger.info(f"Logistic Regression scross val score : {scores.mean()}")
+    exit(0);
 
